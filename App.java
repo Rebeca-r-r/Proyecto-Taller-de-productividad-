c718c0039043329199d8066fe418f0a78fdfbef6
Rebeca-r-r <rebeca_180793@hotmail.com>
2025-12-01 22:41:13 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+package com.miempresa.inventario;
+
+public class App {
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Inventario - versión de ejemplo");
+    }
+}
---
+++ AppTest.java
@@ -0,0 +1,12 @@
+package com.miempresa.inventario;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void pruebaSumaSimple() {
+        assertEquals(5, App.sumar(2, 3));
+    }
+}
